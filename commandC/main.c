eb8d5948e52ba960cb23d96b005df560036f4dca
chendw <cmcc@chendwdembp.workgroup>
2014-07-16 11:25:51 +0800

    Initial Commit

---
+++ commandC/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  commandC
+//
+//  Created by chendw on 14-7-16.
+//  Copyright (c) 2014年 CHENDW. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
